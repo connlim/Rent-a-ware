e7560445574232c779158ba922bdb1584f5eb243
AlbertZhang <relativisticlagrangian@gmail.com>
2018-08-25 03:54:15 +0800

    chatclient

--- client/src/components/Messaging.vue
+++ client/src/components/Messaging.vue
@@ -1,15 +1,62 @@
-<template>
-  <div class="">
-    
-  </div>
-</template>
-
 <script>
+import { actions } from './store';
+import Card from 'chatcomponents/card';
+import List from 'chatcomponents/list';
+import Text from 'chatcomponents/text';
+import Message from 'chatcomponents/message';
 export default {
-  name: 'Messaging'
+    components: { Card, List, Text, Message },
+    vuex: {
+        actions: actions
+    },
+    created () {
+        this.initData();
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<template>
+<div id="app">
+    <div class="sidebar">
+        <card></card>
+        <list></list>
+    </div>
+    <div class="main">
+        <message></message>
+        <text></text>
+    </div>
+</div>
+</template>
+
+<style lang="less" scoped>
+#app {
+    margin: 20px auto;
+    width: 800px;
+    height: 600px;
+    overflow: hidden;
+    border-radius: 3px;
+    .sidebar, .main {
+        height: 100%;
+    }
+    .sidebar {
+        float: left;
+        width: 200px;
+        color: #f4f4f4;
+        background-color: #2e3238;
+    }
+    .main {
+        position: relative;
+        overflow: hidden;
+        background-color: #eee;
+    }
+    .text {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+    }
+    .message {
+        height: ~'calc(100% - 160px)';
+    }
+}
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -5,11 +5,13 @@ import '@mdi/font/css/materialdesignicons.min.css'
 import 'fullcalendar/dist/fullcalendar.css'
 import App from './App.vue'
 import FullCalendar from 'vue-full-calendar'
+import store from './store';
 
 Vue.config.productionTip = false
 Vue.use(Buefy, { defaultIconPack: 'mdi' })
 Vue.use(FullCalendar)
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  store: store
 }).$mount('#app')
