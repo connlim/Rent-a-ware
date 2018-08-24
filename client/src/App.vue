ed2645be2dc81372a0b13be26eb5c518f6fc1367
shihern <shihern2708@gmail.com>
2018-08-24 22:44:13 +0800

    Add landing page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing />
   </div>
 </template>
 
@@ -16,7 +15,7 @@ import Search from './components/Search.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
@@ -26,8 +25,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- client/src/components/Landing.vue
+++ client/src/components/Landing.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="">
-    
-  </div>
+  <section class="hero is-fullheight has-bg-img">
+    <div class="hero-head">
+      <NavbarTransparent />
+    </div>
+    <div class="hero-body">
+      <div class="container margin-bottom-60">
+        <div class="columns is-fullhd">
+          <div class="column is-6 is-offset-2">
+            <div class="title has-text-white">
+              Rent unique equipment and experiences all over Singapore.
+            </div>
+          </div>
+        </div>
+        <div class="columns is-fullhd">
+          <div class="column is-8 is-offset-2">
+            <div class="subtitle">
+              <b-field>
+                <b-input size="is-large" placeholder="What would you like to rent today?"></b-input>
+              </b-field>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import NavbarTransparent from './NavbarTransparent.vue'
+
 export default {
-  name: 'Landing'
+  name: 'Landing',
+  components: {
+    NavbarTransparent
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.margin-bottom-60 {
+  margin-bottom: 60px;
+}
+.has-bg-img { 
+  background: linear-gradient(0deg,rgba(70,70,70,0.3),rgba(70, 70, 70, 0.3)), url('../assets/background.jpg') center center; 
+  background-size: cover; 
+}
+
 </style>
