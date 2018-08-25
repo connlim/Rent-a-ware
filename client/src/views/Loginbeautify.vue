967c62ddbee51c41ac7247e4758ffcaddaa22f3a
AlbertZhang <relativisticlagrangian@gmail.com>
2018-08-25 13:15:15 +0800

    .

--- client/src/views/Loginbeautify.vue
+++ client/src/views/Loginbeautify.vue
@@ -2,15 +2,14 @@
     <div class="login-page">
   <div class="form">
     <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
+      <input type="text" name="Username" v-model="input.username" placeholder="Username"/>
+      <input type="password" name="Password" v-model="input.password" placeholder="Password"/>
       <button v-on:click="register()">create</button>
       <p class="message">Already registered? <a href="#">Sign In</a></p>
     </form>
     <form class="login-form">
       <input type="text" name="Username" v-model="input.username" placeholder="Username"/>
-      <input type="password" name="Password" v-model="input.username" placeholder="Password"/>
+      <input type="password" name="Password" v-model="input.password" placeholder="Password"/>
       <button v-on:click="login()">login</button>
       <p class="message">Not registered? <a href="#">Create an account</a></p>
     </form>
