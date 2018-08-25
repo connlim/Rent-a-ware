d87ffdb8b9f17bfda7345170886d3acc887dc3ba
Enigmatrix <enigmatrix2000@gmail.com>
2018-08-25 10:35:40 +0800

    api stuff

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -3,9 +3,10 @@ import {get, post} from './rest';
 export default {
     /*
      * ware is: {
+     *      _id: ObjectId,
      *      name: String,
      *      tags: [String],
-     *      seller: { type: Schema.Types.ObjectId, ref: "User" },
+     *      seller: { type: ObjectId, ref: "User" },
      *      location: String,
      *      price: Number,
      *      description: String,
@@ -13,7 +14,39 @@ export default {
      * }
      */
     createWare: async (ware) => {
-        await post("/api/wares/create", ware);
+        return await post("/api/wares/create", ware);
     },
-
+    findWareById: async (wareId) => {
+        return await get(`/api/wares/${wareId}`);
+    },
+    /*
+     * deal is {
+     *      ware: ObjectId,
+     *      buyerSlotProposal: [{start: Date, end: Date}]
+     * }
+     */
+    createDeal: async (deal) => {
+        return await post("api/deals/create", deal);
+    },
+    /*
+     * Returns [{
+     *      _id: ObjectId,
+     *      name: String,
+     *      tags: [String],
+     *      seller: { type: ObjectId, ref: "User" },
+     *      location: String,
+     *      price: Number,
+     *      description: String,
+     *      slots: [{start: Date, end: Date}]
+     * }]
+     */
+    search: async (text) => {
+        return await get(`api/search?q=${text}`);
+    },
+    buyerConfirm: async (dealId) => {
+        await post(`api/deals/${dealId}/buyer-confirm`);
+    },
+    sellerConfirm: async (dealId) => {
+        await post(`api/deals/${dealId}/seller-confirm`);
+    },
 }
